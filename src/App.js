1aa8f826b148bc137e73d7e9f0dff911d3bb9a86
fidaaaaa <fidakummali@gmail.com>
2023-03-16 15:11:06 +0530

    section navbar started

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import LeftNavBar from './components/includes/LeftNavBar';
 
 function App() {
-  return 
+  return (
+    <> 
+      <LeftNavBar/>
+      
+    </>
+    
+  )
+  
 }
 
 export default App;
--- src/components/includes/LeftNavBar.js
+++ src/components/includes/LeftNavBar.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function LeftNavBar() {
   return (
-    <div>LeftNavBar</div>
+    <>
+        <SectionMain>
+            <SectionWrapper>
+                <SectionTop>
+                    <Logo>
+                        <LogoImg/>
+                    </Logo>
+                </SectionTop>
+                <SectionMiddle>
+                    <Ul>
+                        <Li>
+                            <LeftLogoContainer>
+                                <LeftLogoImage/>
+                            </LeftLogoContainer>
+                            <LinkNav></LinkNav>
+                        </Li>
+                        <Li>
+                            <LeftLogoContainer>
+                                <LeftLogoImage/>
+                            </LeftLogoContainer>
+                            <LinkNav></LinkNav>
+                        </Li>
+                        <Li>
+                            <LeftLogoContainer>
+                                <LeftLogoImage/>
+                            </LeftLogoContainer>
+                            <LinkNav></LinkNav>
+                        </Li>
+                        <Li>
+                            <LeftLogoContainer>
+                                <LeftLogoImage/>
+                            </LeftLogoContainer>
+                            <LinkNav></LinkNav>
+                        </Li>
+                        <Li>
+                            <LeftLogoContainer>
+                                <LeftLogoImage/>
+                            </LeftLogoContainer>
+                            <LinkNav></LinkNav>
+                        </Li>
+                        <LiLast>
+                            <LeftLogoContainer>
+                                <LeftLogoImage/>
+                            </LeftLogoContainer>
+                            <LinkNav></LinkNav>
+                        </LiLast>
+                    </Ul>
+                </SectionMiddle>
+                <SectionBottom>
+                    <Div>
+                        <NavImage/>
+                    </Div>
+                    <BuyPremium>Buy Premium</BuyPremium>
+                    <LearnMore>Learn More</LearnMore>
+                </SectionBottom>
+            </SectionWrapper>
+        </SectionMain>
+    </>
+    
   )
 }
+
+const SectionMain = styled.section``;
+const SectionWrapper = styled.section``;
+const SectionTop = styled.section``;
+const Logo = styled.h1``;
+const LogoImg = styled.img``;
+const SectionMiddle = styled.section``;
+const Ul = styled.ul``;
+const Li = styled.li``;
+const LiLast = styled.section``;
+const LeftLogoContainer = styled.section``;
+const LeftLogoImage = styled.img``;
+const LinkNav = styled.section``;
+const SectionBottom = styled.section``;
+const Div = styled.div``;
+const NavImage = styled.img``;
+const BuyPremium = styled.button``;
+const LearnMore = styled.(Link)``;
+
+
+
+
+
+
 
 export default LeftNavBar
