c83abc349e050457a506de091f43eb57a0e90b33
fidaaaaa <fidakummali@gmail.com>
2023-03-16 16:54:41 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import LeftNavBar from './components/includes/LeftNavBar';
+import Header from './components/includes/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <> 
+  return <Router>
+    <Container>
       <LeftNavBar/>
+      <Wrapper>
+        <Header/>
+      </Wrapper>
+    </Container>
+  </Router>
+   
       
-    </>
+      
     
-  )
+    
+  
   
 }
+const Container = styled.section`
+    display: flex;
+    align-items: start;
+    justify-content: space-between;
+`;
+const Wrapper = styled.div`
+    width: 83%;
+`;
+
 
 export default App;
--- src/components/includes/LeftNavBar.js
+++ src/components/includes/LeftNavBar.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Link, NavLink} from 'react-router-dom';
+import Logos from '../Assets/Images/trophy.svg';
+import Logos1 from '../Assets/Images/notes.svg';
+import Logos2 from '../Assets/Images/star.svg';
+import Logos3 from '../Assets/Images/rocket.svg';
+import Logos4 from '../Assets/Images/Food-Site.svg';
+
 
 function LeftNavBar() {
   return (
@@ -8,52 +15,52 @@ function LeftNavBar() {
             <SectionWrapper>
                 <SectionTop>
                     <Logo>
-                        <LogoImg/>
+                        <LogoImg src={require("../Assets/Images/logo-comp.png")} />
                     </Logo>
                 </SectionTop>
                 <SectionMiddle>
                     <Ul>
                         <Li>
                             <LeftLogoContainer>
-                                <LeftLogoImage/>
+                                <LeftLogoImage src={Logos}/>
                             </LeftLogoContainer>
-                            <LinkNav></LinkNav>
+                            <LinkNav className={({ isActive }) => isActive ? "active" : ""} to="/">Home</LinkNav>
                         </Li>
                         <Li>
                             <LeftLogoContainer>
-                                <LeftLogoImage/>
+                                <LeftLogoImage src={Logos4}/>
                             </LeftLogoContainer>
-                            <LinkNav></LinkNav>
+                            <LinkNav className={({ isActive }) => isActive ? "active" : ""} to="scheduler">Scheduler</LinkNav>
                         </Li>
                         <Li>
                             <LeftLogoContainer>
-                                <LeftLogoImage/>
+                                <LeftLogoImage src={Logos3}/>
                             </LeftLogoContainer>
-                            <LinkNav></LinkNav>
+                            <LinkNav className={({ isActive }) => isActive ? "active" : ""} to="technology">Technology</LinkNav>
                         </Li>
                         <Li>
                             <LeftLogoContainer>
-                                <LeftLogoImage/>
+                                <LeftLogoImage src={Logos}/>
                             </LeftLogoContainer>
-                            <LinkNav></LinkNav>
+                            <LinkNav className={({ isActive }) => isActive ? "active" : ""} to="rewards">Rewards</LinkNav>
                         </Li>
                         <Li>
                             <LeftLogoContainer>
-                                <LeftLogoImage/>
+                                <LeftLogoImage src={Logos1}/>
                             </LeftLogoContainer>
-                            <LinkNav></LinkNav>
+                            <LinkNav className={({ isActive }) => isActive ? "active" : ""} to="notes">Notes</LinkNav>
                         </Li>
                         <LiLast>
                             <LeftLogoContainer>
-                                <LeftLogoImage/>
+                                <LeftLogoImage src={Logos2}/>
                             </LeftLogoContainer>
-                            <LinkNav></LinkNav>
+                            <LinkNav className={({ isActive }) => isActive ? "active" : ""} to="submission">Submission</LinkNav>
                         </LiLast>
                     </Ul>
                 </SectionMiddle>
                 <SectionBottom>
                     <Div>
-                        <NavImage/>
+                        <NavImage src={require("../Assets/Images/Rectangle 20.jpg")}/>
                     </Div>
                     <BuyPremium>Buy Premium</BuyPremium>
                     <LearnMore>Learn More</LearnMore>
@@ -65,23 +72,86 @@ function LeftNavBar() {
   )
 }
 
-const SectionMain = styled.section``;
+const SectionMain = styled.section`
+    width: 19%;
+    margin: 4px;
+    box-shadow: 0px 3px 10px 3px #d6cdcdc9;
+    padding-bottom: 90px;
+`;
 const SectionWrapper = styled.section``;
-const SectionTop = styled.section``;
-const Logo = styled.h1``;
-const LogoImg = styled.img``;
+const SectionTop = styled.section`
+    margin: 12px 0 60px;
+`;
+const Logo = styled.h1`
+    width: 25%;
+    margin: 0 auto;
+`;
+const LogoImg = styled.img`
+    width: 100%;
+    display: block;
+`;
 const SectionMiddle = styled.section``;
-const Ul = styled.ul``;
-const Li = styled.li``;
-const LiLast = styled.section``;
-const LeftLogoContainer = styled.section``;
-const LeftLogoImage = styled.img``;
-const LinkNav = styled.section``;
-const SectionBottom = styled.section``;
-const Div = styled.div``;
-const NavImage = styled.img``;
-const BuyPremium = styled.button``;
-const LearnMore = styled.(Link)``;
+const Ul = styled.ul`
+    width: 65%;
+    margin: 0 auto;
+`;
+const Li = styled.li`
+    display: flex;
+    align-items: center;
+    margin-bottom: 48px;
+    color: #9E9696;
+`;
+const LiLast = styled.li`
+    margin-bottom: 0;
+    display: flex;
+    align-items: center;
+    color: #9E9696;
+`;
+const LeftLogoContainer = styled.div`
+    width: 15%;
+    margin-right: 32px;
+`;
+const LeftLogoImage = styled.img`
+     width: 100%;
+     display: block;
+`;
+const LinkNav = styled(NavLink)`
+    color: #9E9696;
+    font-weight: 500;
+    font-size: 15px;
+`;
+const SectionBottom = styled.section`
+    width: 75%;
+    margin: 85px 2px 30px 57px;
+    box-shadow: 0px 3px 3px 2px #d6cdcdc9;
+    text-align: center;
+    border-radius: 4px;
+`;
+const Div = styled.div`
+    width: 40%;
+    margin: 0 auto;
+`;
+const NavImage = styled.img`
+    width:100%;
+    display:block;
+`;
+const BuyPremium = styled.button`
+    display: block;
+    padding: 12px;
+    background: #5C75FB;
+    color: white;
+    border-radius: 6px;
+    font-weight: 600;
+    margin: 24px auto;
+    font-size: 13px;
+`;
+const LearnMore = styled(Link)`
+    color: #5C75FB;
+    font-weight: 600;
+    display: inline-block;
+    margin-bottom: 24px;
+    font-size: 13px;
+`;
 
 
 
