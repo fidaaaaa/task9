231c3379dc9b393523fc8e546ef81df17922cb78
fidaaaaa <fidakummali@gmail.com>
2023-03-17 10:53:25 +0530

    header completed

--- src/components/includes/Header.js
+++ src/components/includes/Header.js
@@ -3,8 +3,63 @@ import styled from 'styled-components';
 
 function Header() {
   return (
-    <div>hello</div>
+    <>
+    <HeaderSection>
+      <Div>
+        <Ul>
+          <FirstLi>
+              <ImageContainer>
+                <Img  src={require('../Assets/Images/man.png')} alt="Image"/>
+              </ImageContainer>
+          </FirstLi>
+          <Li>
+              <ImageContainer>
+                <Img src={require('../Assets/Images/setting.png')} alt="Image"/>
+              </ImageContainer>
+          </Li>
+          <Li>
+              <ImageContainer>
+                <Img src={require('../Assets/Images/bell5.png')} alt="Image"/>
+              </ImageContainer>
+          </Li>
+        </Ul>
+      </Div>
+    </HeaderSection>
+   
+   
+   
+   </>
   )
 }
+const HeaderSection = styled.header`
+    height: 70px;
+   display: flex;
+   align-items: center;
+`;
+const Div = styled.div`
+    width:95%;
+    margin: 0 auto;
 
-export default Header
+`;
+const Ul = styled.ul`
+    display: flex;
+    flex-direction: row-reverse;
+    list-style: none;
+`;
+const Li  = styled.li`
+    margin-right:20px;
+`;
+const FirstLi = styled.li`
+    margin-right: 0;
+`;
+const ImageContainer  = styled.div`
+width: 30px;
+`;
+const Img  = styled.img`
+    width: 100%;
+    display: block;
+    border-radius: 50%;
+ `;
+
+export default Header 
+
